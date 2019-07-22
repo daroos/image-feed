461bfa6f279a7b5f83c007bdee117622b1e703d0
Dariusz Osiadacz <dariusz.osiadacz@synerise.com>
2019-07-22 17:37:16 +0200

    Add avatar component

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Constants } from 'expo';
+
+import Avatar from './components/Avatar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Avatar initials={'FL'} size={35} backgroundColor={'teal'} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

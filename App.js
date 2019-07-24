6df4c4f274ce649475b3e83fe54b23eb312d930b
daroos <darekosiadacz@gmail.com>
2019-07-24 22:05:33 +0200

    Add Comments section
    * Add comments section and allow for adding new comments

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import React from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View, Modal } from 'react-native';
 import { Constants } from 'expo';
 
 import Feed from './screens/Feed';
+import Comments from './screens/Comments';
 
-const items = [
-    { id: 0, author: 'Bob Ross' },
-    { id: 1, author: 'Chuck Norris' },
-];
+export default class App extends React.Component {
+    state = {
+        commentsForItem: {},
+        showModal: false,
+        selectedItemId: null,
+    };
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Feed style={styles.feed} />
-    </View>
-  );
+    openCommentScreen = id => {
+        this.setState({
+            showModal: true,
+            selectedItemId: id,
+        });
+    };
+
+    closeCommentScreen = () => {
+        this.setState({
+            showModal: false,
+            selectedItemId: null,
+        });
+    };
+
+    onSubmitComment = (text) => {
+        const { selectedItemId, commentsForItem } = this.state;
+        const comments = commentsForItem[selectedItemId] || [];
+
+        const updated = {
+            ...commentsForItem,
+            [selectedItemId]: [...comments, text],
+        };
+        this.setState({ commentsForItem: updated });
+    };
+
+  render() {
+      const { commentsForItem, showModal, selectedItemId } = this.state;
+
+      return (
+          <View style={styles.container}>
+              <Feed
+                  style={styles.feed}
+                  commentsForItem={commentsForItem}
+                  onPressComments={this.openCommentScreen}
+              />
+              <Modal
+                  visible={showModal}
+                  animationType="slide"
+                  onRequestClose={this.closeCommentScreen}
+              >
+                  <Comments
+                      style={styles.comments}
+                      comments={commentsForItem[selectedItemId] || []}
+                      onClose={this.closeCommentScreen}
+                      onSubmitComment={this.onSubmitComment}
+                  />
+              </Modal>
+          </View>
+      );
+  }
 }
 
 const platformVersion =
@@ -31,5 +78,12 @@ const styles = StyleSheet.create({
         Platform.OS === 'android' || platformVersion < 11
             ? Constants.statusBarHeight
             : 0,
+    },
+    comments: {
+        flex: 1,
+        marginTop:
+            Platform.OS === 'android' || platformVersion < 11
+                ? Constants.statusBarHeight
+                : 0,
     },
 });
